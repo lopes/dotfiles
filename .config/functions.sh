b065da17c0ad48f312ca9445229c379587eb14c4
Joe Lopes <jose.lopes@nubank.com.br>
2024-06-05 15:21:33 -0300

    revamped built-in functions

--- .config/functions.sh
+++ .config/functions.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-function ping-ts() {
+function pingts() {
+    # timestamped ping
     ping "$1" | xargs -L 1 -I '{}' date '+%H:%M:%S - {}'
+}
+
+function cheat() {
+    # cheat.sh wrapper    
+    curl -s cheat.sh/"$1" | less -R
 }
