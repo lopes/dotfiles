919b3b21b884b9702a113ffbea02f2a73181f3ff
Joe Lopes <2897144+lopes@users.noreply.github.com>
2025-01-29 16:02:19 -0300

    new alias, zed updated

--- .config/aliases.bsd.sh
+++ .config/aliases.bsd.sh
@@ -33,5 +33,6 @@ alias myip="ipconfig getifaddr en0 && dig +short txt ch whoami.cloudflare @1.0.0
 
 alias pwgen="openssl rand -base64 256 | tr -d '\n' | head -c"
 alias pyvenv="{[ ! -d "venv" ] && python3 -m venv venv}; source venv/bin/activate"
+alias missing="grep -v -F -x -f" # usage: missing f1 f2 -> lines in f2 and not in f1
 
 alias docker="/Applications/Docker.app/Contents/Resources/bin/docker"  # docker on macOS
